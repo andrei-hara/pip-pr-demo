e2d5f19b2a4a01ef6de2566844e035575948cee9
Taciuc Marian <liviu-marian.taciuc@stundent.tuiasi.ro>
2021-03-06 12:34:11 +0200

    Modificare funtie Adunare

--- src/demo/HelloWorld.java
+++ src/demo/HelloWorld.java
@@ -1,12 +1,23 @@
 package demo;
 
 public class HelloWorld {
+	
+	static int adunare(int x, int y){
+		return x+y;
+	}
 
 	public static void main(String[] args) {
+		
+		int x,y;
+		x=3;
+		y=7;
+		
+		System.out.println(+adunare(x,y));
 		
 		System.out.println("Hello World");
 		
 		System.out.println("Test 1");
+		
 
 	}
 
